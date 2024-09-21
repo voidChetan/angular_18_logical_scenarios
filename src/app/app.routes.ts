db9e11fb63192dee51230c025ac80cb66ff7845f
codefirst2022 <codefirst2022@gmail.com>
2024-09-21 17:48:07 +0530

    Model Popup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ModalPopupComponent } from './pages/modal-popup/modal-popup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'moddal-popup',
+        component: ModalPopupComponent
+    }
+];
